f573c0946626cac67292f5bd28440af295f8b00c
ohmycloud <ohmycloudy@gmail.com>
2024-10-21 21:33:32 +0800

    add empty course vec

--- tutor-nodb/src/state.rs
+++ tutor-nodb/src/state.rs
@@ -1,8 +1,12 @@
 use std::sync::Mutex;
+use crate::model::Course;
 
 pub struct AppState {
     /// Shared immutable state
     pub health_check_response: String,
     /// Shared mutable state
     pub visit_count: Mutex<u32>,
+    /// Courses are stored in application state as a Vec collection
+    /// protected by a Mutex
+    pub courses: Mutex<Vec<Course>>,
 }
