73d0afcf9813d18d9401893901594dd0d8472b1f
ohmycloud <ohmycloudy@gmail.com>
2024-10-22 00:48:30 +0800

    fix app_state

--- tutor-nodb/src/model.rs
+++ tutor-nodb/src/model.rs
@@ -5,8 +5,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Course {
-    pub tutor_id: i32,
-    pub course_id: Option<i32>,
+    pub tutor_id: u32,
+    pub course_id: Option<u32>,
     pub course_name: String,
     pub posted_time: Option<NaiveDateTime>,
 }
--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use crate::handlers::{health_check_handler, new_course};
+use crate::handlers::{get_courses_for_tutor, health_check_handler, new_course};
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
@@ -9,5 +9,6 @@ pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/courses")
             .route("/", web::post().to(new_course))
+            .route("/{tutor_id}", web::get().to(get_courses_for_tutor)),
     );
 }
