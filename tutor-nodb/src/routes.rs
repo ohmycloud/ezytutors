2e62980260b3074922466ee2678438f62da598cc
ohmycloud <ohmycloudy@gmail.com>
2024-10-22 23:36:12 +0800

    get detail course

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use crate::handlers::{get_courses_for_tutor, health_check_handler, new_course};
+use crate::handlers::{get_course_detail, get_courses_for_tutor, health_check_handler, new_course};
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
@@ -9,6 +9,7 @@ pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/courses")
             .route("/", web::post().to(new_course))
-            .route("/{tutor_id}", web::get().to(get_courses_for_tutor)),
+            .route("/{tutor_id}", web::get().to(get_courses_for_tutor))
+            .route("/{tutor_id}/{course_id}", web::get().to(get_course_detail)),
     );
 }
