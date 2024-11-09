0580a214807d78131925d13108578753e8068308
ohmycloud <ohmycloudy@gmail.com>
2024-11-09 21:46:57 +0800

    revamping the project structure

---
+++ tutor-db/src/bin/iter5.rs
@@ -0,0 +1,42 @@
+#[path = "../iter5/dbaccess/mod.rs"]
+mod db_access;
+
+#[path = "../iter5/errors.rs"]
+mod errors;
+#[path = "../iter5/handlers/mod.rs"]
+mod handlers;
+#[path = "../iter5/models/mod.rs"]
+mod models;
+#[path = "../iter5/routes.rs"]
+mod routes;
+#[path = "../iter5/state.rs"]
+mod state;
+
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use routes::*;
+use sqlx::PgPool;
+use state::AppState;
+use std::sync::Mutex;
+use std::{env, io};
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "Rakulang Rocks!".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+    // Start HTTP server
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
